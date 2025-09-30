6dff2e2178b2f409460e63c3b9a57db449849f64
DiegoSS14 <sousadiego533@gmail.com>
2025-09-30 15:35:29 -0300

    Add functions removeItemCart and totalResultCart on CartContext

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -4,7 +4,9 @@ import { type ProductsProps } from '../pages/home';
 interface ContextData {
     cart: CartProps[];
     amount: number;
-    addItemCart: (newItem: ProductsProps) => void
+    addItemCart: (newItem: ProductsProps) => void;
+    removeItemCart: (itemEdit: CartProps) => void;
+    total: number | undefined;
 }
 
 interface CartProps {
@@ -25,6 +27,7 @@ export const CartContext = createContext({} as ContextData)
 
 function CartProvider({ children }: CartProviderProps) {
     const [cart, setCart] = useState<CartProps[]>([])
+    const [total, setTotal] = useState<number> (0)
 
     function addItemCart(newItem: ProductsProps) {
         const convertItem: CartProps = {
@@ -36,14 +39,15 @@ function CartProvider({ children }: CartProviderProps) {
             amount: 0,
             total: 0,
         }
-        const IndexItemCart = cart.findIndex(item => item.id === convertItem.id)
+        const indexItemCart = cart.findIndex(item => item.id === convertItem.id)
 
-        if (IndexItemCart !== -1) {
-            let cartList = cart
+        if (indexItemCart !== -1) {
+            let cartList = [...cart]
 
-            cartList[IndexItemCart].amount = cartList[IndexItemCart].amount + 1;
-            cartList[IndexItemCart].total = cartList[IndexItemCart].total * cartList[IndexItemCart].amount;
+            cartList[indexItemCart].amount = cartList[indexItemCart].amount + 1;
+            cartList[indexItemCart].total = cartList[indexItemCart].price * cartList[indexItemCart].amount;
 
+            totalResultCart(cartList)
             setCart(cartList)
             return
         }
@@ -55,13 +59,39 @@ function CartProvider({ children }: CartProviderProps) {
         }
 
         setCart(products => [...products, data])
+        totalResultCart([...cart, data])
+    }
+
+    function removeItemCart(itemEdit: CartProps) {
+        const indexItemCart = cart.findIndex(item => item.id === itemEdit.id)
+        if(cart[indexItemCart]?.amount > 1) {
+            // Remove 1 amount do item
+            let cartList = [...cart]
+
+            cartList[indexItemCart].amount = cartList[indexItemCart].amount - 1;
+            cartList[indexItemCart].total = cartList[indexItemCart].total - cartList[indexItemCart].price;
+            setCart(cartList)
+            totalResultCart(cartList)
+            return
+        }
+        const cartList = cart.filter(item => item.id != itemEdit.id)
+        setCart(cartList)
+        totalResultCart(cartList)
+    }
+
+    function totalResultCart(items: CartProps[]) {
+        const myCart = items
+        let result = myCart.reduce((acc, obj) => {return acc + obj.total}, 0)
+        setTotal(result)
     }
 
     return (
         <CartContext.Provider value={{
             cart,
             amount: cart.length,
-            addItemCart
+            addItemCart,
+            removeItemCart,
+            total,
         }}>
             {children}
         </CartContext.Provider>
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,15 +1,17 @@
 import { useContext } from 'react'
 import { CartContext } from '../../contexts/CartContext'
+import { Link } from 'react-router'
 
 export function Cart() {
-    const { cart } = useContext(CartContext)
+    const { cart, addItemCart, removeItemCart, total } = useContext(CartContext)
 
     return (
         <div>
             <main className="w-full max-w-7xl flex flex-col items-center justify-center gap-7 mx-auto">
                 <h1 className="text-2xl font-bold mt-6">Meu carrinho</h1>
 
-                <div className="w-full flex flex-col gap-6 px-5">
+                <div className="w-full flex flex-col items-center gap-6 px-5">
+
                     {
                         cart && cart.map(item => (
                             <section
@@ -32,12 +34,14 @@ export function Cart() {
                                 <div className="flex gap-3 items-center justify-center">
                                     <button
                                         className="w-8 h-8 flex items-center justify-center bg-zinc-400 text-white font-bold rounded-lg transform duration-200 hover:bg-red-500 hover:scale-101 cursor-pointer"
+                                        onClick={() => { removeItemCart(item) }}
                                     >-</button>
                                     <span className="font-bold">
                                         {item.amount}
                                     </span>
                                     <button
                                         className="w-8 h-8 flex items-center justify-center bg-zinc-400 text-white font-bold rounded-lg transform duration-200 hover:bg-sky-500 hover:scale-101 cursor-pointer"
+                                        onClick={() => addItemCart(item)}
                                     >+</button>
                                 </div>
 
@@ -51,6 +55,24 @@ export function Cart() {
                         )
                         )
                     }
+
+                    {total !== 0 && (
+                        <div className='w-full flex justify-center'>
+                            <h1 className='font-bold text-center text-lg'>
+                                Total: {total?.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                            </h1>
+                        </div>
+                    )}
+
+                    {total === 0 && (
+                        <div className='w-fit flex flex-col items-center font-bold gap-2 p-6 border border-dashed border-gray-400 rounded-lg'>
+                            <span>Ops, seu carrinho está vazio...</span>
+                            <Link
+                                to='/'
+                                className='flex items-center justify-center h-10 w-fit p-5 bg-gray-600 rounded-lg text-white font-medium'
+                            >Acessar produtos</Link>
+                        </div>
+                    )}
                 </div>
             </main>
         </div>
